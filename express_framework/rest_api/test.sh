23719662966a29f3f278e7e90fa85caa82a1a316
larissaguder <lariguder10@gmail.com>
2020-08-13 23:11:31 -0300

    n ao era o fim

---
+++ express_framework/rest_api/test.sh
@@ -0,0 +1,8 @@
+curl "http://localhost:3000/profile"
+curl -H "Content-Type: application/json" -X POST -d '{"first_name": "Batman", "last_name":"Wayne"}' "http://localhost:3000/profile"
+sleep 2
+curl -H "Content-Type: application/json" -X PUT -d '{"first_name": "SuperMan", "last_name":"Man"}' "http://localhost:3000/profile"
+sleep 2
+curl "http://localhost:3000/profile"
+sleep 2
+curl -X DELETE "http://localhost:3000/profile"
